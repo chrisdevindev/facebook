33dd94a3a500c11c43c3ecd77f6e0ad4b88a5857
Christian <alvescastro00cca13@gmail.com>
2020-11-28 12:04:47 -0300

    Adicionando Row na ToolBar.js

--- components/ToolBar.js
+++ components/ToolBar.js
@@ -5,13 +5,20 @@ import styled from 'styled-components/native';
 const Container = styled.View`
     width: 100%;
     height: 92px;
-    background: orange;
+`
+
+const Row = styled.View`
+    flex-direction: row;
+    background: #ffffff;
+    width: 100%;
+    align-items: center;
+
 `
 
 function ToolBar() {
   return (
     <Container>
-        
+        <Row></Row>
     </Container>
   );
 }
