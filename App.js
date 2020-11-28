5ad69a6e48b05421df5549afc63d3e90bd457c7e
Christian <alvescastro00cca13@gmail.com>
2020-11-28 11:51:36 -0300

    Criando e importando a ToolBar

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { ScrollView, StatusBar } from "react-native";
 
 import styled from "styled-components/native";
 import AppBar from "./components/AppBar";
+import ToolBar from "./components/ToolBar";
 
 const Container = styled.SafeAreaView`
   flex: 1;
@@ -16,6 +17,7 @@ const App = () => {
       <Container>
         <ScrollView>
           <AppBar />
+          <ToolBar/>
         </ScrollView>
       </Container>
     </>
---
+++ components/ToolBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import styled from 'styled-components/native';
+
+const Container = styled.View`
+    width: 100%;
+    height: 92px;
+    background: orange;
+`
+
+function ToolBar() {
+  return (
+    <Container>
+        
+    </Container>
+  );
+}
+
+export default ToolBar;
