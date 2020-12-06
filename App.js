4b83b2cbecda73c3e595da0a542f82dabf22a840
Christian <alvescastro00cca13@gmail.com>
2020-12-06 12:12:55 -0300

    Criação do novo componente Story.js

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import styled from "styled-components/native";
 import AppBar from "./components/AppBar";
 import ToolBar from "./components/ToolBar";
 import Users from "./components/Users";
+import Story from "./components/Story";
 
 const Container = styled.SafeAreaView`
   flex: 1;
@@ -20,6 +21,7 @@ const App = () => {
           <AppBar />
           <ToolBar/>
           <Users/>
+          <Story/>
         </ScrollView>
       </Container>
     </>
---
+++ components/Story.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components/native'
+
+const Container = styled.View`
+    width: 100%;
+    height: 192px;
+    background: blue;
+`
+
+const Story = () => {
+    return (
+        <Container></Container>
+    )
+}
+
+export default Story
