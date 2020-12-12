3437fbbec1f3302326054cc97547ed90f7c9ca7d
Christian <alvescastro00cca13@gmail.com>
2020-12-12 13:00:14 -0300

    Adicionando uma View no Feed

--- components/Feed.js
+++ components/Feed.js
@@ -1,18 +1,45 @@
 import React from 'react'
-
+import { View } from 'react-native'
 import styled from 'styled-components/native'
+import Avatar from './Avatar'
 
 const Container = styled.View`
     flex: 1;
     height: 100px;
     background: orange;
 `
+const Header = styled.View`
+    height: 50px;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    margin-top: 6px;
+    padding: 0 11px; 
+`
+const Row = styled.View`
+    align-items: center;
+    flex-direction: row;
+`
+const User = styled.Text`
+    font-size: 12px;
+    font-weight: bold;
+    color: #222121;
+`
 
 const Feed = () => {
     return (
         <>
             <Container>
-
+                <Header>
+                    <Row>
+                        <Avatar 
+                            source={require('../assets/user3.jpg')}
+                        />
+                        <View style={{paddingLeft: 10}}>
+                            <User>Christian Castro</User>
+                        </View>
+                    </Row>
+                </Header>
             </Container>
         </>
     )
